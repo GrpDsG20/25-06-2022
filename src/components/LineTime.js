bdfcf72b980b70ad8b29cc1bb921a36a1a3ae612
SmitheSinlaE <dixonzarzosa@gmail.com>
2022-06-25 00:52:20 -0500

    first commit

---
+++ src/components/LineTime.js
@@ -0,0 +1,82 @@
+import './LineTime.sass';
+
+
+export function Line(){
+
+
+    return(
+      <>
+<h1>Historia Imcetron, <span>1910–2000</span></h1>
+<div class="flex-parent">
+	<div class="input-flex-container">
+		<input type="radio" name="timeline-dot" data-description="1910"/>
+		<div class="dot-info" data-description="1910">
+			<span class="year">2013</span>
+			<span class="label">headset</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1920"/>
+		<div class="dot-info" data-description="1920">
+			<span class="year">2014</span>
+			<span class="label">jungle gym</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1930"/>
+		<div class="dot-info" data-description="1930">
+			<span class="year">2015</span>
+			<span class="label">chocolate chip</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1940"/>
+		<div class="dot-info" data-description="1940">
+			<span class="year">2016</span>
+			<span class="label">Jeep</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1950"/>
+		<div class="dot-info" data-description="1950">
+			<span class="year">2017</span>
+			<span class="label">leaf blower</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1960"/>
+		<div class="dot-info" data-description="1960">
+			<span class="year">2018</span>
+			<span class="label">magnetic stripe card</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1970"/>
+		<div class="dot-info" data-description="1970">
+			<span class="year">2019</span>
+			<span class="label">wireless LAN</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1980"/>
+		<div class="dot-info" data-description="1980">
+			<span class="year">2020</span>
+			<span class="label">flash memory</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="1990"/>
+		<div class="dot-info" data-description="1990">
+			<span class="year">2021</span>
+			<span class="label">World Wide Web</span>
+		</div>
+		<input type="radio" name="timeline-dot" data-description="2000"/>
+		<div class="dot-info" data-description="2000">
+			<span class="year">2022</span>
+			<span class="label">Google AdWords</span>
+		</div>
+		<div id="timeline-descriptions-wrapper">
+			<p data-description="1910">And future Call of Duty players would thank them.</p>
+			<p data-description="1920">Because every kid should get to be Tarzan for a day.</p>
+			<p data-description="1930">And the world rejoiced.</p>
+			<p data-description="1940">Because building roads is inconvenient.</p>
+			<p data-description="1950">Ain’t nobody got time to rake.</p>
+			<p data-description="1960">Because paper currency is for noobs.</p>
+			<p data-description="1970">Nobody likes cords. Nobody.</p>
+			<p data-description="1980">Brighter than glow memory.</p>
+			<p data-description="1990">To capitalize on an as-yet nascent market for cat photos.</p>
+			<p data-description="2000">Because organic search rankings take work.</p>
+		</div>
+	</div>
+</div>
+
+
+    
+  </>
+    
+    );
+  }
